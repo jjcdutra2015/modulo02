5bc998c4bb3a8fb527610c0f3b097698d9ba5c12
Julio Dutra <jjcdutra@gmail.com>
2019-10-17 16:19:07 -0300

    Cadastro de usuários

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Julio Dutra',
-    email: 'julio@rocketseat.com.br',
-    password_hash: '1112225678',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
