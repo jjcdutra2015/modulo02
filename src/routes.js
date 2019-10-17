9a78c64bc59edf24d35985d68f037ae95639694a
Julio Dutra <jjcdutra@gmail.com>
2019-10-17 15:50:46 -0300

    Criando loader de models

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Julio Dutra',
+    email: 'julio@rocketseat.com.br',
+    password_hash: '1112225678',
+  });
+
+  return res.json(user);
 });
 
 export default routes;
