06c6004eae934bcb6a18638eccea9b9376bb0e1b
Julio Dutra <jjcdutra@gmail.com>
2019-10-22 17:12:46 -0300

    Update de usuário

--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -16,8 +16,8 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
-  } catch (error) {
-    return res.status(400).json({ error: 'Token invalid.' });
+  } catch (err) {
+    return res.status(401).json({ error: 'Token invalid.' });
   }
 
   return next();
