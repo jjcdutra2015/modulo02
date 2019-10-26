5027253e3be381eb2084f693e82b10a710194c58
Julio Dutra <jjcdutra@gmail.com>
2019-10-26 10:53:34 -0300

    Listando prestadores de serviços

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
 import multer from 'multer';
+
 import multerConfig from './config/multer';
+import authMiddleware from './app/middlewares/auth';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
-
-import authMiddleware from './app/middlewares/auth';
+import ProviderController from './app/controllers/ProviderController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,4 +21,5 @@ routes.put('/users', UserController.update);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
+routes.get('/providers', ProviderController.index);
 export default routes;
