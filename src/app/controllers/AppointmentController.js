5b10069dd03ed7efc201f0f5e9081f355ae353cb
Julio Dutra <jjcdutra@gmail.com>
2019-11-01 21:16:09 -0300

    Cancelamento de agendamento

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,4 +1,4 @@
-import { startOfHour, parseISO, isBefore, format } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format, subHours } from 'date-fns';
 import pt from 'date-fns/locale/pt';
 import * as Yup from 'yup';
 import Appointment from '../models/Appointment';
@@ -57,11 +57,11 @@ class AppointmentController {
         .json({ error: 'You can only create appointment with provider' });
     }
 
-    // if (isProvider.id === req.userId) {
-    //   return res
-    //     .status(400)
-    //     .json({ error: 'Provider is not create appointment for himself' });
-    // }
+    if (isProvider.id === req.userId) {
+      return res
+        .status(400)
+        .json({ error: 'Provider is not create appointment for himself' });
+    }
 
     const hourStart = startOfHour(parseISO(date));
 
@@ -100,6 +100,30 @@ class AppointmentController {
       content: `Novo agendamento de ${user.name} para ${formattedDate}`,
       user: provider_id,
     });
+
+    return res.json(appointment);
+  }
+
+  async delete(req, res) {
+    const appointment = await Appointment.findByPk(req.params.id);
+
+    if (appointment.user_id !== req.userId) {
+      return res.status(401).json({
+        error: "You don't have permission to cancel this appointment.",
+      });
+    }
+
+    const dateWithSub = subHours(appointment.date, 2);
+
+    if (isBefore(dateWithSub, new Date())) {
+      return res
+        .status(401)
+        .json({ error: 'You can only cancel appointments 2 hours advance.' });
+    }
+
+    appointment.canceled_at = new Date();
+
+    await appointment.save();
 
     return res.json(appointment);
   }
--- src/routes.js
+++ src/routes.js
@@ -22,15 +22,17 @@ routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
 
+routes.get('/providers', ProviderController.index);
+
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedule', ScheduleController.index);
 
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
 
-routes.get('/schedule', ScheduleController.index);
-
 routes.post('/files', upload.single('file'), FileController.store);
 
-routes.get('/providers', ProviderController.index);
 export default routes;
