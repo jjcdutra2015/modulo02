d6b929f5ec7a9ae119ff77c640c3cc9d876631e0
Julio Dutra <jjcdutra@gmail.com>
2019-11-12 21:39:18 -0300

    Variáveis de ambiente

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'c14a1507929b89',
-    pass: '646dc8e73e5700',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarbr <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://ea29d28870a44ff39ed849b0be84d84b@sentry.io/1817837',
+  dsn: process.env.SENTRY_DSN,
 };
